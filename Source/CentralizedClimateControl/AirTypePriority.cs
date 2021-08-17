cf790ceff0ac9f700cf113f70347d5ae337d79e2
Adirelle <adirelle@gmail.com>
2021-08-17 12:28:39 +0200

    Added extensions to AirFlowPriority.
    this gives it a more object-y behavior.

--- Source/CentralizedClimateControl/AirTypePriority.cs
+++ Source/CentralizedClimateControl/AirTypePriority.cs
@@ -7,4 +7,42 @@ namespace CentralizedClimateControl
         Frozen = 2,
         Auto = 3
     }
+
+    public static class AirTypePriorityExtensions
+    {
+        private const string SwitchPipeAutoKey = "CentralizedClimateControl.Command.SwitchPipe.Auto";
+        private const string SwitchPipeRedKey = "CentralizedClimateControl.Command.SwitchPipe.Red";
+        private const string SwitchPipeBlueKey = "CentralizedClimateControl.Command.SwitchPipe.Blue";
+        private const string SwitchPipeCyanKey = "CentralizedClimateControl.Command.SwitchPipe.Cyan";
+
+        public static string CommandLabelKey(this AirTypePriority priority)
+        {
+            return priority switch {
+                AirTypePriority.Hot => SwitchPipeRedKey,
+                AirTypePriority.Cold => SwitchPipeBlueKey,
+                AirTypePriority.Frozen => SwitchPipeCyanKey,
+                _ => SwitchPipeAutoKey,
+            };
+        }
+
+        public static string CommandIconName(this AirTypePriority priority)
+        {
+            return priority switch {
+                AirTypePriority.Hot => "UI/PipeSelect_Red",
+                AirTypePriority.Cold => "UI/PipeSelect_Blue",
+                AirTypePriority.Frozen => "UI/PipeSelect_Cyan",
+                _ => "UI/PipeSelect_Auto",
+            };
+        }
+
+        public static AirTypePriority Next(this AirTypePriority priority)
+        {
+            return priority switch {
+                AirTypePriority.Hot => AirTypePriority.Cold,
+                AirTypePriority.Cold => AirTypePriority.Frozen,
+                AirTypePriority.Frozen => AirTypePriority.Auto,
+                _ => AirTypePriority.Hot,
+            };
+        }
+    }
 }
--- Source/CentralizedClimateControl/CentralizedClimateControlUtility.cs
+++ Source/CentralizedClimateControl/CentralizedClimateControlUtility.cs
@@ -6,11 +6,6 @@ namespace CentralizedClimateControl
 {
     public static class CentralizedClimateControlUtility
     {
-        private const string SwitchPipeAutoKey = "CentralizedClimateControl.Command.SwitchPipe.Auto";
-        private const string SwitchPipeRedKey = "CentralizedClimateControl.Command.SwitchPipe.Red";
-        private const string SwitchPipeBlueKey = "CentralizedClimateControl.Command.SwitchPipe.Blue";
-        private const string SwitchPipeCyanKey = "CentralizedClimateControl.Command.SwitchPipe.Cyan";
-
         /// <summary>
         ///     Get the Network Manager of the Map
         /// </summary>
@@ -29,63 +24,13 @@ namespace CentralizedClimateControl
         public static Command_Action GetPipeSwitchToggle(CompAirFlowConsumer compAirFlowConsumer)
         {
             var currentPriority = compAirFlowConsumer.AirTypePriority;
-            Texture2D icon;
-            string label;
-
-            switch (currentPriority)
-            {
-                case AirTypePriority.Auto:
-                    label = SwitchPipeAutoKey.Translate();
-                    icon = ContentFinder<Texture2D>.Get("UI/PipeSelect_Auto");
-                    break;
-
-                case AirTypePriority.Hot:
-                    label = SwitchPipeRedKey.Translate();
-                    icon = ContentFinder<Texture2D>.Get("UI/PipeSelect_Red");
-                    break;
-                case AirTypePriority.Cold:
-                    label = SwitchPipeBlueKey.Translate();
-                    icon = ContentFinder<Texture2D>.Get("UI/PipeSelect_Blue");
-                    break;
-                case AirTypePriority.Frozen:
-                    label = SwitchPipeCyanKey.Translate();
-                    icon = ContentFinder<Texture2D>.Get("UI/PipeSelect_Cyan");
-                    break;
-
-                default:
-                    label = SwitchPipeAutoKey.Translate();
-                    icon = ContentFinder<Texture2D>.Get("UI/PipeSelect_Auto");
-                    break;
-            }
 
             return new Command_Action {
-                defaultLabel = label,
+                defaultLabel = currentPriority.CommandLabelKey().Translate(),
                 defaultDesc = "CentralizedClimateControl.Command.SwitchPipe.Desc".Translate(),
                 hotKey = KeyBindingDefOf.Misc4,
-                icon = icon,
-                action = delegate {
-                    switch (currentPriority)
-                    {
-                        case AirTypePriority.Auto:
-                            compAirFlowConsumer.SetPriority(AirTypePriority.Hot);
-                            break;
-
-                        case AirTypePriority.Hot:
-                            compAirFlowConsumer.SetPriority(AirTypePriority.Cold);
-                            break;
-
-                        case AirTypePriority.Cold:
-                            compAirFlowConsumer.SetPriority(AirTypePriority.Frozen);
-                            break;
-                        case AirTypePriority.Frozen:
-                            compAirFlowConsumer.SetPriority(AirTypePriority.Auto);
-                            break;
-
-                        default:
-                            compAirFlowConsumer.SetPriority(AirTypePriority.Auto);
-                            break;
-                    }
-                }
+                icon = ContentFinder<Texture2D>.Get(currentPriority.CommandIconName()),
+                action = delegate { compAirFlowConsumer.SetPriority(currentPriority.Next()); }
             };
         }
     }
