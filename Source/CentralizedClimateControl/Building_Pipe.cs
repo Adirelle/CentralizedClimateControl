b03e120dd9bba9b507d3ad1ec7f03e4472f53e2c
Adirelle <adirelle@gmail.com>
2021-08-18 15:13:13 +0200

    Fix the Building_Pipe.

--- Source/CentralizedClimateControl/Building_Pipe.cs
+++ Source/CentralizedClimateControl/Building_Pipe.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace CentralizedClimateControl
 {
-    class Building_Pipe
+    public class Building_Pipe : Building
     {
+        public override Graphic Graphic => graphic;
+
+        private Graphic graphic;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            var pipe = GetComp<CompPipe>();
+            graphic = GraphicsLoader.GetPipeGraphic(pipe.FlowType, pipe.Hidden);
+        }
     }
 }
