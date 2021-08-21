7ec2a2e23219971e00a944d5fda9fc752e253538
Adirelle <adirelle@gmail.com>
2021-08-21 09:47:15 +0200

    Simplify PreferredFlowTypeGizmo a bit.

--- Source/CentralizedClimateControl/PreferredFlowTypeGizmo.cs
+++ Source/CentralizedClimateControl/PreferredFlowTypeGizmo.cs
@@ -13,41 +13,46 @@ namespace CentralizedClimateControl
     {
         private CompBuilding parent;
 
-        private FlowType nextFlowType;
+        private FlowType NextFlowType => parent.PreferredFlowType switch
+        {
+            FlowType.Hot => FlowType.Cold,
+            FlowType.Cold => FlowType.Frozen,
+            FlowType.Frozen => FlowType.Any,
+            _ => FlowType.Hot,
+        };
 
         public PreferredFlowTypeGizmo(CompBuilding parent) : base()
         {
             this.parent = parent;
 
             // @TODO: translate
-            defaultDesc = "Click to change color affinity".Translate();
+            defaultDesc ="Click to change color affinity".Translate();
             hotKey = KeyBindingDefOf.Misc4;
         }
 
         public void NotifyChange()
         {
             icon = Graphics.PreferredFlowTypeIcons(parent.PreferredFlowType);
-
-            (defaultLabel, nextFlowType) = parent.PreferredFlowType switch
+            defaultLabel = parent.PreferredFlowType switch
             {
                 // @TODO: translate
-                FlowType.Hot => ("Red only".Translate(), FlowType.Cold),
+                FlowType.Hot => "Red only".Translate(),
 
                 // @TODO: translate
-                FlowType.Cold => ("Blue only".Translate(), FlowType.Frozen),
+                FlowType.Cold => "Blue only".Translate(),
 
                 // @TODO: translate
-                FlowType.Frozen => ("Cyan only".Translate(), FlowType.Any),
+                FlowType.Frozen => "Cyan only".Translate(),
 
                 // @TODO: translate
-                _ => ("Auto".Translate(), FlowType.Hot),
+                _ => "Auto".Translate(),
             };
         }
 
         public override void ProcessInput(Event ev)
         {
             base.ProcessInput(ev);
-            parent.SetPreferredFlowType(nextFlowType);
+            parent.SetPreferredFlowType(NextFlowType);
         }
     }
 }
