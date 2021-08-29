ad42bf4fb7adfd1e49dc28a39d1383bd969f7ce0
Adirelle <adirelle@gmail.com>
2021-08-29 19:57:47 +0200

    Avoid possible issues with sed scripts in update-About.sh.

--- .scripts/update-About.sh
+++ .scripts/update-About.sh
@@ -2,11 +2,11 @@
 set -eu
 tmpFile=$(mktemp)
 (
-    sed -e '0,/<description>/'!d About/About.xml
+    sed -e '0,/<description>/!d' About/About.xml
     echo '<![CDATA['
     pandoc -t .pandoc/SteamBBCode.lua README.md
     echo ']]>'
-    sed -e '\x</description>x,$'!d About/About.xml
+    sed -e '\x</description>x,$!d' About/About.xml
 ) >"$tmpFile"
 
 mv "$tmpFile" About/About.xml
